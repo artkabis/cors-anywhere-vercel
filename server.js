40d3818b5b4d5d1c3b8995936c7a74909b22bc9c
Artkabis <artkabis@live.fr>
2023-06-01 13:36:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ function parseEnvList(env) {
 
 // Set up rate-limiting to avoid abuse of the public CORS Anywhere server.
 var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
-
+console.log('_____________________________________________________',{checkRateLimit});
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
